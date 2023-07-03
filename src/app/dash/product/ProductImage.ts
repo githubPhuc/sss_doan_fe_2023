d9a909a67666287bb05145b341b61180aa458b8e
githubPhuc <0306191061@caothang.edu.vn>
2023-07-03 09:43:00 +0700

    k

--- src/app/dash/product/ProductImage.ts
+++ src/app/dash/product/ProductImage.ts
@@ -1,8 +1,7 @@
-export class Product {
+export class Image {
     constructor(
         public Id :number,
         public nameImage :string,
         public idProduct :string,
         public PathImage :number,
-       
     ){}}
